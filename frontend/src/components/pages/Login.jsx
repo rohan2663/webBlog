8a013d662a5cc8e62732ff5f688a560e8298c12e
Rohan Kumar <142005785+rohan2663@users.noreply.github.com>
2024-10-14 16:11:38 +0530

    Update Login.jsx

--- frontend/src/components/pages/Login.jsx
+++ frontend/src/components/pages/Login.jsx
@@ -15,7 +15,7 @@ const Login = () => {
     e.preventDefault();
     await axios
       .post(
-        "https://webblog-1-6fkk.onrender.com/api/v1/user/login",
+        "http://localhost:4000/api/v1/user/login",
         { email, password, role },
         {
           withCredentials: true,
