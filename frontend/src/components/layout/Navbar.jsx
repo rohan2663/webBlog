d5abc49618a7dd741ce6611d3f33a4d2f128bca3
Rohan Kumar <142005785+rohan2663@users.noreply.github.com>
2024-10-14 14:18:19 +0530

    Update Navbar.jsx

--- frontend/src/components/layout/Navbar.jsx
+++ frontend/src/components/layout/Navbar.jsx
@@ -24,7 +24,7 @@ const Navbar = () => {
     e.preventDefault();
     try {
       const { data } = await axios.get(
-        "http://localhost:4000/api/v1/user/logout",
+        "https://webblog-1-6fkk.onrender.com/api/v1/user/logout",
         { withCredentials: true }
       );
       setIsAuthenticated(false);
