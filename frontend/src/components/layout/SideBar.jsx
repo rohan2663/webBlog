a2a6b7af3c2b20398331ce73770188f37615272e
Rohan Kumar <142005785+rohan2663@users.noreply.github.com>
2024-10-14 16:09:53 +0530

    Update SideBar.jsx

--- frontend/src/components/layout/SideBar.jsx
+++ frontend/src/components/layout/SideBar.jsx
@@ -17,7 +17,7 @@ const SideBar = ({ setComponent }) => {
     e.preventDefault();
     try {
       const { data } = await axios.get(
-        "https://webblog-1-6fkk.onrender.com/api/v1/user/logout",
+        "http://localhost:4000/api/v1/user/logout",
         { withCredentials: true }
       );
       setIsAuthenticated(false);
