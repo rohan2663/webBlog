7b92815996c77f4b90280364a32460da240062e3
Rohan Kumar <142005785+rohan2663@users.noreply.github.com>
2024-10-14 16:11:23 +0530

    Update AllAuthors.jsx

--- frontend/src/components/pages/AllAuthors.jsx
+++ frontend/src/components/pages/AllAuthors.jsx
@@ -9,7 +9,7 @@ const AllAuthors = () => {
   useEffect(() => {
     const fetchAuthors = async () => {
       const { data } = await axios.get(
-        "https://webblog-1-6fkk.onrender.com/api/v1/user/authors",
+        "http://localhost:4000/api/v1/user/authors",
         { withCredentials: true }
       );
       setAuthors(data.authors);
